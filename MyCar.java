48ae2c3844c590560dc5d81272664e58eac03f7d
elenaneo <57110448+elenaneo@users.noreply.github.com>
2019-11-18 22:11:53 +0500

    Create MyCar.java

---
+++ MyCar.java
@@ -0,0 +1,18 @@
+public class MyCar {
+    String code;
+    String gosNumber;
+    float probeg;
+    int dop;
+    public MyCar(String code, String gosNumber, float probeg, int dop)
+    {
+        this.code=code;
+        this.gosNumber=gosNumber;
+        this.probeg=probeg;
+        this.dop=dop;
+    }
+    public MyCar()
+    {
+     }
+
+
+}
