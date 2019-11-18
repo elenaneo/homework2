27e1009b2c26629fdbf34bbbbc10904aabfcffc3
elenaneo <57110448+elenaneo@users.noreply.github.com>
2019-11-18 22:11:05 +0500

    Create CodeCar.java

---
+++ CodeCar.java
@@ -0,0 +1,17 @@
+public class CodeCar {
+    String code;
+    String name;
+    float price;
+    float normaFuel;
+    public CodeCar (String code, String name,float price,float normaFuel)
+    {
+        this.code=code;
+        this.name=name;
+        this.price=price;
+        this.normaFuel=normaFuel;
+
+    }
+
+
+
+}
